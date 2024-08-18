4dc58f9a76ecba1c222419fd366b4c51c0904142
Hiroshi McBride <>
2024-08-18 22:58:46 +0200

    wiggly slug

---
+++ src/player_container.gd
@@ -0,0 +1,7 @@
+class_name Player extends Node3D
+
+@export var lookat:Node3D
+@export var camera_3d:Camera3D
+
+func get_lookat():
+	return lookat
--- src/player_slug.gd
+++ src/player_slug.gd
@@ -1,15 +1,22 @@
-extends Node3D
+extends CharacterBody3D
 
-@export var camera_3d: Camera3D
+var camera_3d: Camera3D
 @export var oozle_speed: float
 @export_range(-1,1) var oozle_sigmoid_cutoff: float
-@onready var path_3d: Path3D = $SlugBody
 
 var input_vector : Vector2
 var relative_input_vector : Vector3
 
+func _ready() -> void:
+	var p = get_parent()
+	assert(p is Player)
+	
+	camera_3d = p.camera_3d
+
 func _process(delta: float) -> void:
 	input_vector = Input.get_vector("move_left", "move_right", "move_up", "move_down")
+
+func _physics_process(delta: float) -> void:
 	var forward = camera_3d.global_basis.z
 	var right = camera_3d.global_basis.x
 	
@@ -17,27 +24,22 @@ func _process(delta: float) -> void:
 	#var input_vector_y_forward_only = input_vector.y * _sigmoid(input_vector.y)
 	#if input_vector_y_forward_only > oozle_sigmoid_cutoff: input_vector_y_forward_only = 0
 	
-	relative_input_vector = forward * -Input.get_action_strength("move_up") + right * input_vector.x
-	relative_input_vector *= oozle_speed
+	relative_input_vector = forward * input_vector.y + right * input_vector.x
 	relative_input_vector.y = 0.0
-
-func _physics_process(delta: float) -> void:
-	var curve = path_3d.curve
-	var point0 = curve.get_point_position(0)
-	var point1 = curve.get_point_position(1)
-	var point2 = curve.get_point_position(2)
+	var new_speed = velocity.lerp(relative_input_vector * oozle_speed, 0.9)
+	velocity = Vector3(new_speed.x, 0.0, new_speed.z)
+	move_and_slide()
+	if input_vector.length_squared() > 0:
+		var dir = Vector2(velocity.x, velocity.z)
+		var target_angle = -atan2(velocity.z, velocity.x) - deg_to_rad(90)
+		rotation.y = lerp_angle(rotation.y, target_angle, delta * 10.0)
 	
-	curve.set_point_position(0, point0 + relative_input_vector * 5 * delta)
-	curve.set_point_position(1, curve.sample_baked(0.5))
-	curve.set_point_position(2, curve.sample_baked(1))
+	#print(velocity)
 	
-	#var new_speed = velocity.lerp(relative_input_vector * SPEED, 0.9)
-	#velocity = Vector3(new_speed.x, 0.0, new_speed.z)
-	#move_and_slide()
 
 func _sigmoid(x:float) -> float:
 	return 1 / (1 + pow(2.71828, x))
 
 
 func get_lookat():
-	return $SlugBody/Body10
+	return $SlugBody/Head
