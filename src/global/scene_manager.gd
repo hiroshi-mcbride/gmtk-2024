7dcb50093d27b50b216a227dfd10463bea327beb
Lea <leavdcruijsen@gmail.com>
2024-08-20 14:22:55 +0200

    fixed

--- src/global/scene_manager.gd
+++ src/global/scene_manager.gd
@@ -18,7 +18,6 @@ func _on_game_started():
 	load_scene(WORLD_SCENE, self)
 	load_scene(PAUSE_MENU_SCENE, canvas_layer)
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
-	%MenuAmbience.stop()
 	
 	GlobalSignals.game_quit.connect(_on_game_quit)
 
